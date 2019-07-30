4bf45bd29bcbd59cbb10aba4003991e741668b2b
Vio <Vio@DESKTOP-UFEKKTR>
2019-07-30 20:57:16 +0300

    homework1

--- ConsoleApp3file/Program.cs
+++ ConsoleApp3file/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,26 @@ namespace ConsoleApp3file
 {
     class Program
     {
+        private const string FileName = "data.txt";
+        private const string IDFile = "IDS.txt";
         static void Main(string[] args)
         {
+            try
+            {
+.
+                if (File.Exists(IDFile))
+                {
+                    // daca avem deja fisierul
+                    File.Delete(IDFile);
+                }
+
+                // Create the file.
+                using (FileStream CODELIST = File.Create(IDFile))
+                {
+                    StreamReader streamReader = 
+                }
+                catch
+
         }
     }
 }
