73779045c7ec44bf68d254e9802befd5bc1c5af7
viorelaBudianu <viorela.hariga@yahoo.com>
2019-08-05 11:24:51 +0300

    ParallelLetter

---
+++ ParallelLetter/ParallelLetter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelLetter
+{
+    public static class ParallelLetter
+    {
+        public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+        {
+            var dict = new ConcurrentDictionary<char, int>();
+            //var dict = new Dictionary<char, int>();
+
+            var lockObject = new object();
+            Parallel.ForEach(texts, text =>
+            {
+                Parallel.ForEach(text.ToLower(), letter =>
+                {
+                    lock (lockObject)
+                    {
+                        if (IsValidCharacter(letter))
+                        {
+                            if (!dict.ContainsKey(letter))
+                            {
+
+                                dict.TryAdd(letter, 1);
+                            }
+                            else
+                            {
+                                dict[letter]++;
+                            }
+                        }
+                    }
+                });
+            });
+
+            return dict.ToDictionary(k => k.Key, v => v.Value);
+        }
+
+        private static bool IsValidCharacter(char text)
+        {
+            return char.IsLetter(text) && !char.IsNumber(text);
+        }
+    }
+}
---
+++ ParallelLetter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace ParallelLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cuvant1 = new string[] { "ANA", "ARE", "MERE" };
+            var rezultat = ParallelLetter.Calculate(cuvant1);
+            foreach (var e in rezultat)
+            {
+                Console.WriteLine($"{e.Key} apare de {e.Value} ori");
+            }
+        }
+    }
+}
